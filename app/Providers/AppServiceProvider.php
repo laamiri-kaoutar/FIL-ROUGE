e2b488e80f9cec250a2074969d05035111e5806b
laamiri-kaoutar <kaoutarlaamiri355@gmail.com>
2025-04-21 22:10:19 +0000

    Bind ReviewRepository in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,14 @@
 namespace App\Providers;
 
 use App\Repositories\TagRepository;
+use App\Repositories\ReviewRepository;
 use App\Repositories\ServiceRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\CategoryRepository;
 use App\Interfaces\TagRepositoryInterface;
 use App\Interfaces\ServiceRepositoryInterface;
 use App\Interfaces\CategoryRepositoryInterface;
+use App\Interfaces\ReviewRepositoryInterface;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(TagRepositoryInterface::class, TagRepository::class);
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
         $this->app->bind(ServiceRepositoryInterface::class, ServiceRepository::class);
+        $this->app->bind(ReviewRepositoryInterface::class, ReviewRepository::class);
+
 
 
     }
