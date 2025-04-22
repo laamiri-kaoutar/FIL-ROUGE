02c682871e2531bcf3b2bd795d68856cf6a65566
laamiri-kaoutar <kaoutarlaamiri355@gmail.com>
2025-04-22 08:44:24 +0000

    Add favorites method to ClientController and update toggleFavorite for redirect

--- resources/views/client/reviews.blade.php
+++ resources/views/client/reviews.blade.php
@@ -69,8 +69,8 @@
                         @endforeach
                     </div>
                 @else
-                    <!-- Empty state -->
-                    <div id="emptyState" class="p-8 text-center">
+
+                <div id="emptyState" class="p-8 text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mx-auto text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                         </svg>
